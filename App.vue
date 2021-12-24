9d48d250fad87a68d5060adc16aa34d98258e863
ilyas <ilyas.sultanov2020@list.ru>
2021-12-24 21:56:45 +0500

    first commit

---
+++ App.vue
@@ -0,0 +1,48 @@
+<template>
+  <div id="app">
+    <h1>{{ title }}</h1>
+    <p>Welcome...</p>
+    <div v-if="showModal">
+      <Modal :header="header" :text="text" theme="sale" @close="toggleModal" />
+    </div>
+    <button @click="toggleModal">open modal</button>
+  </div>
+</template>
+
+<script>
+import Modal from "./components/Modal.vue";
+export default {
+  name: "App",
+  components: { Modal },
+  data() {
+    return {
+      title: "My First App :)",
+      header: "Singup for Giveaway",
+      text: "Lorem ipsum dolor",
+      showModal: false,
+    };
+  },
+  methods: {
+    toggleModal() {
+      this.showModal = !this.showModal;
+    },
+  },
+};
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+
+h1 {
+  border-bottom: 1px solid #ddd;
+  display: inline-block;
+  padding-bottom: 10px;
+}
+</style>
